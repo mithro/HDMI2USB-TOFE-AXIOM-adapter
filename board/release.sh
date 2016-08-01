261656844be451336f830580d78f8fa0d8f61917
Tim 'mithro' Ansell <mithro@mithis.com>
2016-08-01 22:54:14 +1000

    Adding a release script.

---
+++ board/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]; then
+	echo "Dirty repo, commit before running."
+	exit 1
+fi
+
+VERSION=$(git describe --long)
+
+echo "Current version: $VERSION"
+
+# Work out the next version
+OUTDIR=gerbers/$VERSION/
+mkdir $OUTDIR
+
+# Update the version embedded in the PCB
+sed -e"s/(gr_text [\$]Desc[\$]/(gr_text \"$VERSION\"/" --in-place=.bak TOFE-AXIOM-adapter.kicad_pcb
+
+# Generate the gerber files
+python ../external/gen_gerber_and_drill_files_board.py TOFE-AXIOM-adapter.kicad_pcb $OUTDIR/
+git add $OUTDIR/*
+
+git commit -m "Creating gerbers $VERSION for TOFE-AXIOM-adapter"
+
+cd $OUTDIR/
+ZIP="TOFE-AXIOM-adapter-$VERSION.zip"
+zip -r ../$ZIP .
+
+cd ..; md5sum $ZIP
+
+git reset --hard
+
+exit 0
